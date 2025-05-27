6f8b400b1a5ce2fcca347e2533ed3d49b56dc390
Owl <danilsereda7@gmail.com>
2025-05-28 00:39:08 +0300

    сделаны  YRD-120 - YRD-121

--- src/main/java/com/yourday/project/backend/controller/UserController.java
+++ src/main/java/com/yourday/project/backend/controller/UserController.java
@@ -1,23 +1,46 @@
 package com.yourday.project.backend.controller;
 import com.yourday.project.backend.entity.User;
+import com.yourday.project.backend.security.JwtUtil;
+import com.yourday.project.backend.service.RegistrationService;
 import com.yourday.project.backend.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
+
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @GetMapping("/get")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    @PostMapping("/update")
+    public ResponseEntity<?> updateUser(@RequestBody User userUpdate, HttpServletRequest request) {
+
+        String token = jwtUtil.extractTokenFromRequest(request);
+        String userEmail = jwtUtil.extractEmail(token);
+
+        User user = userService.findByEmail(userEmail);
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
+        }
+
+        userService.updateUser(user, userUpdate);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/yourday/project/backend/service/UserService.java
+++ src/main/java/com/yourday/project/backend/service/UserService.java
@@ -2,14 +2,20 @@ package com.yourday.project.backend.service;
 import com.yourday.project.backend.entity.User;
 import com.yourday.project.backend.interfase.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -23,6 +29,25 @@ public class UserService {
 
         return userRepository.findByEmail(userEmail)
                 .orElseThrow(() -> new IllegalArgumentException("User with email " + userEmail + " not found"));
+    }
+
+    public void updateUser(User user, User updatedUser) {
+        if (user == null || updatedUser == null) {
+            throw new IllegalArgumentException("User objects cannot be null");
+        }
+
+        Optional.ofNullable(updatedUser.getEmail())
+                .filter(e -> !e.isBlank())
+                .ifPresent(user::setEmail);
+
+
+
+        Optional.ofNullable(updatedUser.getName())
+                .filter(n -> !n.isBlank())
+                .ifPresent(user::setName);
+
+        user.setUpdated_at(LocalDateTime.now());
+        userRepository.save(user);// Обновляем метку изменения
     }
 
 }
